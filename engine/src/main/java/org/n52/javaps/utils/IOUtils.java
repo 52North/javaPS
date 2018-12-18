f37f158e9c67e35fcc19937c54c53976bcf47a1d
Benjamin Pross <b.pross@52north.org>
2018-12-18 09:15:38 +0100

    Remove unnecessary import

--- engine/src/main/java/org/n52/javaps/utils/IOUtils.java
+++ engine/src/main/java/org/n52/javaps/utils/IOUtils.java
@@ -26,7 +26,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
