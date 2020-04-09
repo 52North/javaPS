43140428cc6e9e1f3d59ea176f90738f35a7f8ba
Martin Pontius <m.pontius@52north.org>
2020-04-09 15:51:16 +0200

    Automatic header

--- rest/src/main/java/org/n52/javaps/rest/model/ValueType.java
+++ rest/src/main/java/org/n52/javaps/rest/model/ValueType.java
@@ -1,17 +1,12 @@
 /*
- * Copyright (C) 2020 by 52 North Initiative for Geospatial Open Source Software GmbH
- *
- * Contact: Andreas Wytzisk
- * 52 North Initiative for Geospatial Open Source Software GmbH
- * Martin-Luther-King-Weg 24
- * 48155 Muenster, Germany
- * info@52north.org
+ * Copyright 2016-2020 52°North Initiative for Geospatial Open Source
+ * Software GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
