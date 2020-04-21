f4d64771e040cc8b3e46e3448046716827fc80b1
Benjamin Proß <b.pross@52north.org>
2020-04-21 11:52:17 +0200

    Fix checks 🤦

--- rest/src/main/java/org/n52/javaps/rest/model/NameReferenceType.java
+++ rest/src/main/java/org/n52/javaps/rest/model/NameReferenceType.java
@@ -1,17 +1,12 @@
 /*
- * Copyright (C) 2019 by 52 North Initiative for Geospatial Open Source Software GmbH
- *
- * Contact: Andreas Wytzisk
- * 52 North Initiative for Geospatial Open Source Software GmbH
- * Martin-Luther-King-Weg 24
- * 48155 Muenster, Germany
- * info@52north.org
+ * Copyright 2016-2020 52°North Initiative for Geospatial Open Source
+ * Software GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,94 +24,79 @@ import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
 /**
  * NameReferenceType
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-04-01T13:54:06.047Z[GMT]")
-public class NameReferenceType   {
-  @JsonProperty("name")
-  private String name = null;
+@javax.annotation.Generated(
+        value = "io.swagger.codegen.v3.generators.java.SpringCodegen",
+        date = "2020-04-01T13:54:06.047Z[GMT]")
+public class NameReferenceType {
+    @JsonProperty("name")
+    private String name;
 
-  @JsonProperty("reference")
-  private String reference = null;
+    @JsonProperty("reference")
+    private String reference;
 
-  public NameReferenceType name(String name) {
-    this.name = name;
-    return this;
-  }
+    public NameReferenceType name(String name) {
+        this.name = name;
+        return this;
+    }
 
-  /**
-   * Get name
-   * @return name
-  **/
-      @NotNull
+    /**
+     * Get name
+     *
+     * @return name
+     **/
+    @NotNull
 
     public String getName() {
-    return name;
-  }
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public NameReferenceType reference(String reference) {
-    this.reference = reference;
-    return this;
-  }
+    public NameReferenceType reference(String reference) {
+        this.reference = reference;
+        return this;
+    }
 
-  /**
-   * Get reference
-   * @return reference
-  **/
-  
+    /**
+     * Get reference
+     *
+     * @return reference
+     **/
+
     public String getReference() {
-    return reference;
-  }
-
-  public void setReference(String reference) {
-    this.reference = reference;
-  }
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
+        return reference;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    public void setReference(String reference) {
+        this.reference = reference;
     }
-    NameReferenceType nameReferenceType = (NameReferenceType) o;
-    return Objects.equals(this.name, nameReferenceType.name) &&
-        Objects.equals(this.reference, nameReferenceType.reference);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, reference);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class NameReferenceType {\n");
-    
-    sb.append("    name: ").append(toIndentedString(name)).append("\n");
-    sb.append("    reference: ").append(toIndentedString(reference)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        NameReferenceType nameReferenceType = (NameReferenceType) o;
+        return Objects.equals(this.name, nameReferenceType.name)
+                && Objects.equals(this.reference, nameReferenceType.reference);
     }
-    return o.toString().replace("\n", "\n    ");
-  }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, reference);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("NameReferenceType{name: %s, reference: %s}", name, reference);
+    }
 }
