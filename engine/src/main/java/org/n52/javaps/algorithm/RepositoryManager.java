9bac1683f19e0d8751c296d133a3cc70c3ff4904
Benjamin Proß <b.pross@52north.org>
2021-07-16 10:05:29 +0200

    Adjust company name

--- engine/src/main/java/org/n52/javaps/algorithm/RepositoryManager.java
+++ engine/src/main/java/org/n52/javaps/algorithm/RepositoryManager.java
@@ -1,6 +1,5 @@
 /*
- * Copyright 2016-2021 52°North Initiative for Geospatial Open Source
- * Software GmbH
+ * Copyright 2016-2021 52°North Spatial Information Research GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
